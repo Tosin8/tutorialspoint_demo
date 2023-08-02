6d04dc285a10cda7d7865e56bc8a5815c52e3382
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-03 00:14:15 +0100

    Updating appbar with widgets

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -8,13 +8,27 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        toolbarHeight: 100,
+        // toolbarHeight: 100,
         backgroundColor: Colors.blueAccent,
         elevation: 3,
         title: const Text(
           'Bikey',
           style: TextStyle(color: Colors.white),
         ),
+        leading: const Icon(
+          Icons.home,
+          color: Colors.white,
+        ),
+        actions: const [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.plumbing, color: Colors.white),
+          ),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.shopping_cart, color: Colors.white),
+          ),
+        ],
       ),
       body: Container(
         decoration: const BoxDecoration(
