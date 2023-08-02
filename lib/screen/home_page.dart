6020d53026471b0dfdc5a17d6ab77ce223c820f5
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-03 00:50:29 +0100

    Updating appBar with Drawer

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,36 +7,303 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        // toolbarHeight: 100,
-        backgroundColor: Colors.blueAccent,
-        elevation: 3,
-        title: const Text(
-          'Bikey',
-          style: TextStyle(color: Colors.white),
-        ),
-        leading: const Icon(
-          Icons.home,
-          color: Colors.white,
-        ),
-        actions: const [
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.plumbing, color: Colors.white),
+        appBar: AppBar(
+          shadowColor: Colors.black,
+          centerTitle: true,
+          // toolbarHeight: 100,
+          // toolbarOpacity: 0.5,
+          backgroundColor: Colors.blueAccent,
+          elevation: 3,
+          title: const Text(
+            'Bikey',
+            style: TextStyle(color: Colors.white),
           ),
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.shopping_cart, color: Colors.white),
-          ),
-        ],
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
+          // leading: const Icon(
+          //   Icons.home,
+          //   color: Colors.white,
+          // ),
+          actions: const [
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.favorite_rounded, color: Colors.white),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.shopping_cart, color: Colors.white),
+            ),
+          ],
         ),
-        child: const Center(child: Text('Hello World')),
-      ),
-    );
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: const Center(child: Text('Hello World')),
+        ),
+        drawer: Drawer(
+            backgroundColor: Colors.white,
+            child: SingleChildScrollView(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 100,
+                      color: Colors.green,
+                      child: Row(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          SizedBox(width: 20),
+                          CircleAvatar(
+                            minRadius: 35,
+                            maxRadius: 35,
+                            backgroundColor: Colors.grey,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "Priyanshu",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Divider(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: GestureDetector(
+                        onTap: (() {
+                          // Navigator.pop(
+                          //     context,
+                          //     new MaterialPageRoute(
+                          //         builder: (context) => new HomePageMain()));
+                        }),
+                        child: Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Icon(Icons.home),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Home",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: GestureDetector(
+                        onTap: (() {
+                          // Navigator.push(
+                          //     context,
+                          //     new MaterialPageRoute(
+                          //         builder: (context) => new ExploreMenuPage()));
+                        }),
+                        child: Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Icon(Icons.search),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Explore Menus",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    // SizedBox(
+                    //   height: 10,
+                    // ),
+                    ExpansionTile(
+                      title: Row(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          Icon(CupertinoIcons.person_3_fill,
+                              color: Colors.black),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Vendors",
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black),
+                          )
+                        ],
+                      ),
+
+                      childrenPadding:
+                          EdgeInsets.only(left: 25), // children padding
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: GestureDetector(
+                            onTap: (() {
+                              // Navigator.push(
+                              //     context,
+                              //     new MaterialPageRoute(
+                              //         builder: (context) => new VendorVenuePage()));
+                            }),
+                            child: Row(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Icon(
+                                  CupertinoIcons.waveform_circle_fill,
+                                  color: Colors.black87,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Venue",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black87),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: GestureDetector(
+                            onTap: (() {
+                              // Navigator.push(
+                              //     context,
+                              //     new MaterialPageRoute(
+                              //         builder: (context) =>
+                              //             new VendorPhotographerPage()));
+                            }),
+                            child: Row(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Icon(Icons.camera_alt_outlined),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Photographer",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black87),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: GestureDetector(
+                            onTap: (() {
+                              // Navigator.push(
+                              //     context,
+                              //     new MaterialPageRoute(
+                              //         builder: (context) =>
+                              //             new VendorCenematographyPage()));
+                            }),
+                            child: Row(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Icon(Icons.video_call),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Cinematography",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black87),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: GestureDetector(
+                            onTap: (() {
+                              // Navigator.push(
+                              //     context,
+                              //     new MaterialPageRoute(
+                              //         builder: (context) => new VendorMakeupPage()),);
+                            }),
+                            child: Row(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Icon(Icons.brush),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "Makeup Artists",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black87),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        //more child menu
+                      ],
+                    ),
+
+                    // SizedBox(
+                    //   height: 10,
+                    // ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: GestureDetector(
+                        onTap: (() {
+                          // Navigator.push(
+                          //     context,
+                          //     new MaterialPageRoute(
+                          //         builder: (context) => new DrawerPackages()));
+                        }),
+                        child: Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Icon(Icons.wallet_giftcard_rounded),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Package",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ]),
+            )));
   }
 }
