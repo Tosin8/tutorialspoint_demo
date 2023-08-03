2a3e47dd71a48cf4bf06cc25fd580c6b18059197
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-03 03:04:34 +0100

    Update widget.dart

--- lib/screen/widget.dart
+++ lib/screen/widget.dart
@@ -35,6 +35,8 @@ class Bikey_Drawer extends StatelessWidget {
         title: Text('Live Chat'),
       ),
       const Divider(),
+
+      // My Bikey Account
       const Padding(
         padding: EdgeInsets.all(15.0),
         child: Text(
@@ -54,39 +56,146 @@ class Bikey_Drawer extends StatelessWidget {
         ),
       ),
       ListTile(
-        leading: const Icon(Icons.book),
-        title: const Text(' My Course '),
+        leading: const Icon(Icons.inbox_rounded),
+        title: const Text('Inbox'),
         onTap: () {
           Navigator.pop(context);
         },
       ),
       ListTile(
-        leading: const Icon(Icons.workspace_premium),
-        title: const Text(' Go Premium '),
+        leading: const Icon(Icons.pending_actions_rounded),
+        title: const Text(' Pending Review'),
         onTap: () {
           Navigator.pop(context);
         },
       ),
       ListTile(
-        leading: const Icon(Icons.video_label),
-        title: const Text(' Saved Videos '),
+        leading: const Icon(Icons.countertops_outlined),
+        title: const Text(' Voucher'),
         onTap: () {
           Navigator.pop(context);
         },
       ),
       ListTile(
-        leading: const Icon(Icons.edit),
-        title: const Text(' Edit Profile '),
+        leading: const Icon(Icons.favorite_border_outlined),
+        title: const Text('Saved Item'),
         onTap: () {
           Navigator.pop(context);
         },
       ),
+
+      const Divider(),
+
+      // Category
+
+      const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Text(
+          'Our Categories',
+          style: TextStyle(color: Colors.green),
+        ),
+      ),
+      const InkWell(
+        splashColor: Colors.green,
+        highlightColor: Colors.green,
+        child: ListTile(
+          leading: Icon(Icons.bike_scooter_rounded),
+          title: Text('Mountain Bike'),
+          // onTap: () {
+          //   Navigator.pop(context);
+          // },
+        ),
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text('Folding Bike'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text('Hybrid Bike'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text(' Cruiser Bike'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text('Folding Bike'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text('Road Bike'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+
+// Service
+      Divider(),
+      const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Text(
+          'Our Services',
+          style: TextStyle(color: Colors.green),
+        ),
+      ),
+      const InkWell(
+        splashColor: Colors.green,
+        highlightColor: Colors.green,
+        child: ListTile(
+          leading: Icon(Icons.bike_scooter_rounded),
+          title: Text('Cycling Training'),
+          // onTap: () {
+          //   Navigator.pop(context);
+          // },
+        ),
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text('Repairs'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.bike_scooter_rounded),
+        title: const Text('Spare Parts'),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+
+      Divider(),
       ListTile(
         leading: const Icon(Icons.logout),
         title: const Text('LogOut'),
         onTap: () {
           Navigator.pop(context);
         },
+      ),
+      const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Text(
+          'Contact Us',
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Text(
+          'Help Centre',
+        ),
       ),
     ]));
   }
