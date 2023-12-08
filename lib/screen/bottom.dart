9ba7b5d5dc3cee908aae214bd2323992feb4c5e8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-08 23:59:05 +0100

    Update bottom.dart
    Refactoring

--- lib/screen/bottom.dart
+++ lib/screen/bottom.dart
@@ -1,53 +1,53 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
 
-class BottomNavigation extends StatefulWidget {
-  const BottomNavigation({Key? key}) : super(key: key);
+// class BottomNavigation extends StatefulWidget {
+//   const BottomNavigation({Key? key}) : super(key: key);
 
-  @override
-  State<BottomNavigation> createState() => _BottomNavigationState();
-}
+//   @override
+//   State<BottomNavigation> createState() => _BottomNavigationState();
+// }
 
-class _BottomNavigationState extends State<BottomNavigation> {
-  int _selectedIndex = 0;
-
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-             mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.business),
-                label: 'Business',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.star),
-                label: 'Reviews',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.blue,
-            onTap: _onItemTapped,
-          ),
-        ],
-      ),
+// class _BottomNavigationState extends State<BottomNavigation> {
+//   int _selectedIndex = 0;
 
 
-    );
-  }
-}
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//              mainAxisAlignment: MainAxisAlignment.end,
+//         children: [
+//           BottomNavigationBar(
+//             items: const <BottomNavigationBarItem>[
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.home),
+//                 label: 'Home',
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.business),
+//                 label: 'Business',
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.star),
+//                 label: 'Reviews',
+//               ),
+//             ],
+//             currentIndex: _selectedIndex,
+//             selectedItemColor: Colors.blue,
+//             onTap: _onItemTapped,
+//           ),
+//         ],
+//       ),
+
+
+//     );
+//   }
+// }
