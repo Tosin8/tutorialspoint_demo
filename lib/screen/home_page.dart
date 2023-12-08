d636281ae9724ee24262c2d23f2b2e1037a17705
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-09 00:02:38 +0100

    Update home_page.dart
    Using listView

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -39,58 +39,8 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            Text(
-              'Find your',
-              style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black.withOpacity(0.8)),
-            ),
-            Text(
-              'Bike',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Container(
-                height: 40,
-                //width: 40,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(width: 1, color: Colors.black),
-                    color: Colors.white60),
-                child: const Row(
-                  children: [
-                    SizedBox(width: 10),
-                    Icon(Icons.search),
-                    Text('Search your products'),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Card(
-                child: SizedBox(
-              width: 200,
-              height: 200,
-              child: Column(
-                children: [
-                  Image.asset('assets/images/5.png'),
-                  Text(
-                    'Bikey Cloud',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Text('Mountain Bikes', style: TextStyle(fontSize: 15)),
-                  Text('3000')
-                ],
-              ),
-            )),
-          ],
+        body: ListView(
+          children: [],
         ),
         drawer: const Bikey_Drawer());
   }
