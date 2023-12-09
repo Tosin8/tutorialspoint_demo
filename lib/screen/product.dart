62dbfe48ec003213fe25b325f288b1da55286377
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-09 02:10:23 +0100

    Update product.dart

--- lib/screen/product.dart
+++ lib/screen/product.dart
@@ -1,3 +1,15 @@
 import 'package:scoped_model/scoped_model.dart';
 
-class Product extends Model {}
+class Product extends Model {
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+  int rating;
+
+  Product(
+      {required this.name,
+      required this.description,
+      required this.price,
+      required this.image});
+}
